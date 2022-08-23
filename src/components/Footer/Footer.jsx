0e3b4f76227f4931c88acd2c0e42ecfa803c987c
springboot <nhokxuanquy@gmail.com>
2022-08-23 18:49:55 +0700

    update ui

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='text' style={{
-      fontSize: "16px",
+      fontSize: "1.0em",
       // color: "linear-gradient(238.01deg, rgb(145, 1, 165) -1.4%, rgb(255, 86, 122) 54.09%, rgb(255, 153, 0) 93.78%)",
       textAlign: "center",
       fontWeight: "600"
     }}>
-    <h5>  Created by ABC. © 2022</h5>
+    <h5 style={{fontSize: '0.8em'}}>  Created by dawn team. © 2022</h5>
     </div>
   )
 }
