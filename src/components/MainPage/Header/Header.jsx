8589b5d39941b461ef501370a742972a1f2acbe1
springboot <nhokxuanquy@gmail.com>
2022-08-24 19:49:33 +0700

    fix ui

--- src/components/MainPage/Header/Header.jsx
+++ src/components/MainPage/Header/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         </h1>
         <p className="details">
           It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout.
+          readable content of a page when looking at its layout. 
         </p>
         <div className="header-btns">
           <Link className="cv-btn">Hire Me</Link>
