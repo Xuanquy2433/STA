7c2b31ece09e515873ccd2b53d5591b27c47c508
gp <tranhrp@gmail.com>
2022-08-24 15:32:02 +0700

    gp

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Footer.css'
 export default function Footer() {
   return (
-    <div className='text' style={{
+    <div className='textFt' style={{
       fontSize: "1.0em",
       // color: "linear-gradient(238.01deg, rgb(145, 1, 165) -1.4%, rgb(255, 86, 122) 54.09%, rgb(255, 153, 0) 93.78%)",
       textAlign: "center",
