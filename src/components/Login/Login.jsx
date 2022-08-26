dfd0ccfdf1111aba5f58181735f3e61d390bf2e3
gp <tranhrp@gmail.com>
2022-08-26 11:05:28 +0700

    gp

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-
+import { toast } from 'react-toastify';
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
     const [data, setData] = useState({
