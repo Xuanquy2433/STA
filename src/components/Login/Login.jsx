1a1acb0ba44a2a7bf7dded272f1e34d150bb633f
springboot <nhokxuanquy@gmail.com>
2022-08-26 10:26:01 +0700

    update login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { Link as Link } from 'react-router-dom'
+import { Link  } from 'react-router-dom'
 
 
 export default function Login() {
