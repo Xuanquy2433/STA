69e7fbd03929c0a3d4d7397ec0c7f0cb9e52ca83
springboot <nhokxuanquy@gmail.com>
2022-08-27 10:36:14 +0700

    update

--- src/components/utils/const.js
+++ src/components/utils/const.js
@@ -3,3 +3,5 @@ export const API = "http://localhost:8082";
 export const API_USER_LOGIN = `${API}/user/signIn`;
 
 export const API_USER_SIGNUP = `${API}/user/signup`;
+
+export const API_GET_WALLET = `${API}/wallet/?token=`;
